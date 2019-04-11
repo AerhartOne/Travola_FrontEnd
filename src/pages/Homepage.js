d77c57d4046964026ffc72285daaf86d1bf182f0
Lenox L <lenoxlty1997@gmail.com>
2019-04-11 16:57:20 +0800

    changed jumbotron style in homepage

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -7,14 +7,14 @@ export default class Homepage extends React.Component{
         return(
             <div>
                 <HomepageNav/>
-                <Jumbotron>
-                    <h1 className="display-3">Travola</h1>
-                    <p className="lead">This is a simple hero unit, a simple Jumbotron-style component for calling extra attention to featured content or information.</p>
-                    <hr className="my-2" />
-                    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
-                    <p className="lead">
-                    <Button color="primary">Learn More</Button>
-                    </p>
+                <Jumbotron fluid>
+                    <Container fluid>
+                        <h1 className="display-3">Fluid jumbotron</h1>
+                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
+                        <p className="lead">
+                            <Button color="primary">Learn More</Button>
+                        </p>
+                    </Container>
                 </Jumbotron>
             </div>
         )
