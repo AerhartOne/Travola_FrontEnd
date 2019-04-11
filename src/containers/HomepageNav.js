2f8fa09fb392ad97ade707649613b2caccfd5963
kok huai <kokhuai_liew@hotmail.com>
2019-04-11 17:16:15 +0800

    added login in navbar but form cannot submit

--- src/containers/HomepageNav.js
+++ src/containers/HomepageNav.js
@@ -1,41 +1,40 @@
 import React from 'react';
 import {
-    Collapse,
     Navbar,
-    NavbarToggler,
     NavbarBrand,
     Nav,
     NavItem,
     NavLink,
         } from 'reactstrap';
+import LogIn from '../containers/Login'
 
     export default class HomepageNav extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-        isOpen: false
+        loginModal: false,
         };
     }
-    toggle = () => {
+
+    toggleLogin = () => {
         this.setState({
-        isOpen: !this.state.isOpen
-        });
+            loginModal: !this.state.loginModal
+        })
     }
     render() {
+        const {loginModal} = this.state
         return (
         <div>
             <Navbar className="fixed-top" color="light" light expand="md">
             <NavbarBrand href="/">Travola</NavbarBrand>
-            <NavbarToggler onClick={this.toggle} />
-            <Collapse isOpen={this.state.isOpen} navbar>
                 <Nav className="ml-auto" navbar>
                 <NavItem>
-                    <NavLink>Log In</NavLink>
+                    <NavLink onClick={this.toggleLogin}>Log In</NavLink>
                 </NavItem>
                 </Nav>
-            </Collapse>
             </Navbar>
+            <LogIn loginModal={loginModal} toggleLogin={this.toggleLogin}/>
         </div>
         );
     }
