4d6ab1f3294b07fd443ff159fb44e09a6398975f
kok huai <kokhuai_liew@hotmail.com>
2019-04-11 15:55:09 +0800

    deleted h1 tags

--- src/App.js
+++ src/App.js
@@ -5,14 +5,7 @@ class App extends Component {
   render() {
     return (
       <>
-      <h1>Hello world</h1>
-      <h1>Hello again!</h1>
-      <h1>sdjsdsd</h1>
-      <h1>World Hello</h1>
-      <h1>Hello again!</h1>
-      <h1>sdjsdsd</h1>
-      <h2>hhhhhhh</h2>
-      
+        <h1>Hello world</h1>
       </>
     );
   }
