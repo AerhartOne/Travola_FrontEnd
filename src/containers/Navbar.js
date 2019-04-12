3d720b77ba835842a6a01f62cd97a97396ecbb82
kok huai <kokhuai_liew@hotmail.com>
2019-04-12 14:42:37 +0800

    made changes to the navbar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -5,8 +5,13 @@ import {
     Nav,
     NavItem,
     NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem
         } from 'reactstrap';
 import logo from '../images/travola-logo.png';
+import profile from '../images/anonymous-user.png'
 
 export default class NavBar extends React.Component{
     render(){
@@ -14,19 +19,27 @@ export default class NavBar extends React.Component{
             <>
                 <Navbar className="fixed-top" color="light" light expand="md">
                     <NavbarBrand href="/username"><img src={ logo } alt="travola logo" width="20%"/></NavbarBrand>
-                    <Nav className="ml-auto"navbar>
-                    <NavItem>
-                        <NavLink href="/username">Home</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/username/profile">Profile</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink href="/username/setting">Settings</NavLink>
-                    </NavItem>
-                    <NavItem>
-                        <NavLink>Logout</NavLink>
-                    </NavItem>
+                    <Nav className="ml-auto mr-2"navbar>
+                    <UncontrolledDropdown nav inNavbar>
+                        <DropdownToggle nav caret>
+                            <img src={profile} alt="avatar" width="25px" height="25px" className="rounded-circle"/>
+                        </DropdownToggle>
+                        <DropdownMenu right>
+                        <DropdownItem href="/username">
+                            Home
+                        </DropdownItem>
+                        <DropdownItem href="/username/profile">
+                            Profile
+                        </DropdownItem>
+                        <DropdownItem href="/username/setting">
+                            Account Setting
+                        </DropdownItem>
+                        <DropdownItem divider />
+                        <DropdownItem>
+                            Logout
+                        </DropdownItem>
+                        </DropdownMenu>
+                    </UncontrolledDropdown>
                     </Nav>
                 </Navbar>
             </>
