09d605a92e923b0a7f5fe3b58bdc3ffe02f6b737
Lenox L <lenoxlty1997@gmail.com>
2019-04-12 10:51:35 +0800

    made homepage with sign-in & sign-up button

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,22 +1,28 @@
 import React from 'react'
-import { Jumbotron, Button } from 'reactstrap';
-import HomepageNav from '../containers/HomepageNav'
+import { HashRouter as Router, Route, Link, NavLink } from 'react-router-dom';
+import '../css/homepage.css'
 
 export default class Homepage extends React.Component{
     render(){
         return(
-            <div>
-                <HomepageNav/>
-                <Jumbotron fluid>
-                    <Container fluid>
-                        <h1 className="display-3">Fluid jumbotron</h1>
-                        <p className="lead">This is a modified jumbotron that occupies the entire horizontal space of its parent.</p>
-                        <p className="lead">
-                            <Button color="primary">Learn More</Button>
-                        </p>
-                    </Container>
-                </Jumbotron>
+            
+        <div className="App">
+            <div className="App__Aside"></div>
+            <div className="App__Form">
+                
+                <div className="PageSwitcher">
+                    <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+                    <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+                </div>
+                
+                <div className="FormTitle">
+                    <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+                </div>
+
             </div>
+            
+        </div>
+            
         )
     }
 }
