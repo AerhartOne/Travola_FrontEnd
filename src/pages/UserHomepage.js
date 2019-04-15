fe52a26d5e948d0931a47a7a913d2d2b27a258ae
kok huai <kokhuai_liew@hotmail.com>
2019-04-15 20:11:23 +0800

    Made modal for adding a new trip

--- src/pages/UserHomepage.js
+++ src/pages/UserHomepage.js
@@ -13,8 +13,22 @@ import {
 import '../css/UserHomepage.css'
 import button from '../images/add_button.png'
 import defaults from '../images/default-image.png'
+import NewTripModal from '../containers/NewTripModal'
 
 export default class UserHomepage extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            modal:false
+        }
+    }
+
+    toggle = (e) => {
+        this.setState({
+            modal:!this.state.modal
+        })
+    }
     render(){
         return(
             <div className="body-background">
@@ -35,7 +49,7 @@ export default class UserHomepage extends React.Component{
                     <Col xs="12" lg="4" className="mt-5">
                         <Card className="shadow">
                             <CardBody>
-                                <Button id="add-button" href="/username/dashboard/new">
+                                <Button id="add-button" onClick={this.toggle}>
                                     <CardImg top width="100%" src={button} alt="Card image cap" />
                                     <CardText>Add new trip</CardText>
                                 </Button>
@@ -44,6 +58,7 @@ export default class UserHomepage extends React.Component{
                     </Col>
                 </Row>
             </Container>
+            <NewTripModal modal={this.state.modal} toggle={this.toggle}/>
             </div>
         )
     }
