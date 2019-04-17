59e702b6c94f5d852e039c974ced32b2e5607cb0
Lenox L <lenoxlty1997@gmail.com>
2019-04-17 14:51:10 +0800

    added card in dashboard and remove side bar for the time being

--- src/pages/TripDashboard.js
+++ src/pages/TripDashboard.js
@@ -1,22 +1,70 @@
 import React from 'react'
 import NavBar from '../containers/Navbar'
-import SideBar from '../containers/SideBar'
 import EventAdderButton from '../containers/EventAdderButton'
+import EventCard from '../containers/EventCard'
+import { Container, Row, Col } from 'reactstrap';
 
-import '../css/dashboard.css'
 
 export default class TripDashboard extends React.Component{
     render(){
         return(
-            <div id="db-background">
             
-            <NavBar/>
-            
-            <SideBar/>
+            <div>
+                
+                <Container fluid className="dashboard-container">
+                    
+                    <Row>
+                        
+                        <Col>
+                            <NavBar/>
+                        </Col>
 
-            <EventAdderButton/>
+                    </Row>
+
+                    <Row>
+
+                        <Col>
+                            <EventAdderButton />
+                        </Col>
+                        
+                    </Row>
+                    
+                    <Row className="card-row">
+                        
+                        <Col sm="4">
+                            <EventCard />
+                        </Col>
+
+                        <Col sm="4">
+                            <EventCard />
+                        </Col>
+
+                        <Col sm="4">
+                            <EventCard />
+                        </Col>
+
+                    </Row>
+
+                    <Row className="card-row">
+                        
+                        <Col sm="4">
+                            <EventCard />
+                        </Col>
+
+                        <Col sm="4">
+                            <EventCard />
+                        </Col>
+
+                        <Col sm="4">
+                            <EventCard />
+                        </Col>
+
+                    </Row>
+
+                </Container>
             
             </div>
-        )
+        
+        );
     }
 }
