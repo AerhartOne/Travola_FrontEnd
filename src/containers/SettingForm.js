fb3a1aeeabce6f7374e36ac0dfe9d7e085f00104
kok huai <kokhuai_liew@hotmail.com>
2019-04-18 11:30:12 +0800

    redesigned setting form

--- src/containers/SettingForm.js
+++ src/containers/SettingForm.js
@@ -5,6 +5,7 @@ import {
         Col,
 } from 'reactstrap'
 import '../css/UserSetting.css'
+import '../css/NewTripForm.css'
 
 
 
@@ -16,7 +17,8 @@ class SettingForm extends React.Component {
             first_name: '',
             last_name: '',
             username: '',
-            password: ''
+            password: '',
+            bio:''
         };
 
         this.handleChange = this.handleChange.bind(this);
@@ -35,7 +37,15 @@ class SettingForm extends React.Component {
 
     handleSubmit(e) {
         e.preventDefault();
+        
+        this.setState({
+            first_name: '',
+            last_name: '',
+            username: '',
+            password: '',
+            bio:''
 
+        })
         console.log('The form was submitted with the following data:');
         console.log(this.state);
     }
@@ -53,24 +63,34 @@ class SettingForm extends React.Component {
                                 <div className="formfield">
                                     <label className="formfield_label h4">First Name</label>
                                     <input type="text" name="first_name" className="formfield_input" placeholder="Enter your new first name" value={this.state.first_name} onChange={this.handleChange} />
+                                    <button type="submit" onClick={this.handleSubmit} className="settingfield_button">Change</button>
                                 </div>
 
                                 <div className="formfield">
                                     <label className="formfield_label h4">Last Name</label>
                                     <input type="text" name="last_name" className="formfield_input" placeholder="Enter your new last name" value={this.state.last_name} onChange={this.handleChange} />
+                                    <button type="submit" onClick={this.handleSubmit} className="settingfield_button">Change</button>
                                 </div>
 
                                 <div className="formfield">
                                     <label className="formfield_label h4">Username</label>
                                     <input type="text" name="username" className="formfield_input" placeholder="Enter your new username" value={this.state.username} onChange={this.handleChange} />
+                                    <button type="submit" onClick={this.handleSubmit} className="settingfield_button">Change</button>
                                 </div>
 
                                 <div clasName="formfield">
                                     <label className="formfield_label h4">Password</label>
                                     <input type="password" name="password" className="formfield_input" placeholder="Enter your new password" value={this.state.password} onChange={this.handleChange} />
+                                    <button type="submit" onClick={this.handleSubmit} className="settingfield_button">Change</button>
                                 </div>
 
-                                <button type="submit" className="settingfield_button">Update</button>
+                                <div clasName="formfield">
+                                    <label className="formfield_label h4">Bio</label>
+                                    <input type="text" name="bio" className="formfield_input" placeholder="Change Bio" value={this.state.bio} onChange={this.handleChange} />
+                                    <button type="submit" onClick={this.handleSubmit} className="settingfield_button">Change</button>
+                                </div>
+
+                                
 
                             </form>
                         </Col>
